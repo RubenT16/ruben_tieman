e39ebe3a9cca557baa395175798f8c0dd2d41b39
Ruben <rubent162000@gmail.com>
2023-01-23 12:52:39 +0100

    Added Delete button

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -106,6 +106,10 @@ class PostController extends Controller
      */
     public function destroy(Post $post)
     {
-        //
+        {
+            $post->delete();
+            return to_route('posts.index');
+        }
+        
     }
 }
